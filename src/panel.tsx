44952d566d93edcf2c56ef66eb3b878b07ab95c5
Alex4386 <psh010209@gmail.com>
2023-10-12 02:55:10 +0900

    chore: add filepicker

--- src/panel.tsx
+++ src/panel.tsx
@@ -12,6 +12,7 @@ import {
   ServerAPI,
   showContextMenu,
   staticClasses,
+  Button,
 } from "decky-frontend-lib";
 import { useState, VFC } from "react";
 import { FaCog, FaPlus, FaTimesCircle } from "react-icons/fa";
@@ -26,7 +27,7 @@ const SidePanel: VFC<{
   return <div style={{ boxSizing: 'border-box', width: '100%', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
     {
       appId ? <PanelSection title={'App ID: '+appId}>
-
+        <Button onClick={() => serverAPI.openFilePicker('~')}>Open File</Button>
       </PanelSection> : <div style={{ height: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
         <div className={staticClasses.Title}>Not Selected</div>
         <div>Application was not selected</div>
