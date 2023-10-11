d38cd8e2210f29a854c11f2272864a5afed4ac28
Alex4386 <psh010209@gmail.com>
2023-10-12 04:13:39 +0900

    chore: do game and proton

--- src/panel.tsx
+++ src/panel.tsx
@@ -13,8 +13,9 @@ import {
   showContextMenu,
   staticClasses,
   Button,
+  TextField,
 } from "decky-frontend-lib";
-import { useEffect, useState, VFC } from "react";
+import { useEffect, useRef, useState, VFC } from "react";
 import { FaCog, FaPlus, FaTimesCircle } from "react-icons/fa";
 import ProtonRunnerGlobal from "./common/global";
 
@@ -26,6 +27,9 @@ const SidePanel: VFC<{
 
   const [gamePath, setGamePath] = useState<string | null | undefined>(undefined);
   const [protonPath, setProtonPath] = useState<string | null | undefined>(undefined);
+
+  const [targetPath, setTargetPath] = useState<string | undefined>(undefined);
+  const filePathRef = useRef<typeof TextField | undefined>();
 
   useEffect(() => {
     if (appId && (gamePath === undefined && protonPath === undefined)) {
@@ -52,12 +56,16 @@ const SidePanel: VFC<{
     if (protonData.success) setProtonPath(protonData.result);
   }
 
+  const ModTextField = TextField as any;
+
   return <div style={{ boxSizing: 'border-box', width: '100%', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
     {
       appId ? <PanelSection title={'App ID: '+appId}>
-        <Button onClick={() => serverAPI.openFilePicker('~')}>Open File</Button>
-        <div>Game: {gamePath}</div>
-        <div>Proton: {gamePath}</div>
+        <ModTextField ref={filePathRef} onChange={(e: any) => setTargetPath(e.target.value)} />
+        <Focusable style={{ marginRight: '.5rem', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '1rem' }}>
+          {gamePath && <Button onClick={() => serverAPI.openFilePicker(gamePath)}>Open GamePath</Button>}
+          {gamePath && <Button onClick={() => serverAPI.openFilePicker(protonPath)}>Open ProtonPath</Button>}
+        </Focusable>
       </PanelSection> : <div style={{ height: '100%', display: 'flex', flexDirection: 'row', alignItems: 'stretch' }}>
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
           <div className={staticClasses.Title}>Not Selected</div>
