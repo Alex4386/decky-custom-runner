1ac07c3b81a2c71ee184617218749c182e4d974f
Alex4386 <psh010209@gmail.com>
2023-10-12 02:20:15 +0900

    chore: params

--- src/patches/handler/index.ts
+++ src/patches/handler/index.ts
@@ -1,5 +1,5 @@
 import {
-    afterPatch, ServerAPI, SidebarNavigation
+    afterPatch, ServerAPI, SidebarNavigation, useParams
 } from 'decky-frontend-lib';
 import { ReactElement, VFC } from 'react';
 import ProtonRunnerGlobal from '../../common/global';
@@ -12,7 +12,8 @@ const appPageHandler =
       props.children.props,
       'renderFunc',
       (_: Record<string, any>[], re: ReactElement) => {
-        console.log('renderFuncTriggered!', props, props['appid'], props.path, re);
+        const params = useParams()
+        console.log('renderFuncTriggered!', params, props.path, re);
         return re;
       }
     )
